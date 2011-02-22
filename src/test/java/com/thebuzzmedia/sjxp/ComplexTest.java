bb6cb2183ad4df28bc64c0bcfad4320e752a1a36
Riyad Kalla <software@thebuzzmedia.com>
2011-02-22 12:50:40 -0700

    Finished Complex test case.

--- src/test/java/com/thebuzzmedia/sjxp/ComplexTest.java
+++ src/test/java/com/thebuzzmedia/sjxp/ComplexTest.java
@@ -22,43 +22,58 @@ import com.thebuzzmedia.sjxp.rule.DefaultRule;
 import static junit.framework.Assert.*;
 
 public class ComplexTest extends AbstractTest {
+	public static final String[] ABOUTS = new String[] {
+			"http://news.slashdot.org/story/11/02/21/2210212/Anonymous-Denies-Targeting-Westboro-Baptist-Church?from=rss",
+			"http://ask.slashdot.org/story/11/02/21/215258/Ask-Slashdot-Is-There-a-War-Against-Small-Mail-Servers?from=rss",
+			"http://news.slashdot.org/story/11/02/21/2058243/Physicists-Build-Bigger-Bottles-For-Antimatter?from=rss" };
 	public static final String[] TITLES = new String[] {
-			"Wisconsin’s Ties to Labor Fray as Its Economy Shifts",
-			"American Held in Pakistan Shootings Worked With C.I.A.",
-			"Danger Pent Up Behind Aging Dams" };
-	public static final String[] ORIG_LINKS = new String[] {
-			"http://www.nytimes.com/2011/02/22/us/22union.html?partner=rss&amp;emc=rss",
-			"http://www.nytimes.com/2011/02/22/world/asia/22pakistan.html?partner=rss&amp;emc=rss",
-			"http://www.nytimes.com/2011/02/22/science/22dam.html?partner=rss&amp;emc=rss" };
-	public static final String[] CREATORS = new String[] {
-			"By A. G. SULZBERGER and MONICA DAVEY",
-			"By MARK MAZZETTI, ASHLEY PARKER, JANE PERLEZ and ERIC SCHMITT",
-			"By HENRY FOUNTAIN" };
-	public static final String[] PUB_DATES = new String[] {
-			"Mon, 21 Feb 2011 23:40:19 GMT", "Mon, 21 Feb 2011 23:18:52 GMT",
-			"Mon, 21 Feb 2011 22:20:20 GMT" };
+			"Anonymous Denies Targeting Westboro Baptist Church",
+			"Ask Slashdot: Is There a War Against Small Mail Servers?",
+			"Physicists Build Bigger 'Bottles' For Antimatter" };
+	public static final String[] SUBJECTS = new String[] { "internet",
+			"communications", "news" };
+	public static final String[] DEPTS = new String[] {
+			"listen-to-admiral-ackbar", "lazy-spam-prevention",
+			"go-big-or-go-home" };
 
 	private int count = 0;
 
+	private boolean[] OK_ABOUTS = new boolean[ABOUTS.length];
 	private boolean[] OK_TITLES = new boolean[TITLES.length];
-	private boolean[] OK_ORIG_LINKS = new boolean[ORIG_LINKS.length];
-	private boolean[] OK_CREATORS = new boolean[CREATORS.length];
-	private boolean[] OK_PUB_DATES = new boolean[PUB_DATES.length];
+	private boolean[] OK_SUBJECTS = new boolean[SUBJECTS.length];
+	private boolean[] OK_DEPTS = new boolean[DEPTS.length];
 
 	@Test
 	public void test() {
 		setDebug(true);
-		XMLParser parser = new XMLParser(new TitleRule(), new OrigLinkRule(),
-				new CreatorRule(), new PubDateRule());
+		XMLParser parser = new XMLParser(new AboutRule(), new TitleRule(),
+				new SubjectRule(), new DeptRule());
 		parser.parse(this.getClass().getResourceAsStream(
-				"resources/namespace.xml"));
+				"resources/complex.xml"));
 
-		assertTrue(isAllOK(OK_TITLES, OK_ORIG_LINKS, OK_CREATORS, OK_PUB_DATES));
+		assertTrue(isAllOK(OK_ABOUTS, OK_TITLES, OK_SUBJECTS, OK_DEPTS));
+	}
+
+	class AboutRule extends DefaultRule {
+		public AboutRule() {
+			super(
+					Type.ATTRIBUTE,
+					"/[http://www.w3.org/1999/02/22-rdf-syntax-ns#]RDF/[http://purl.org/rss/1.0/]item",
+					"[http://www.w3.org/1999/02/22-rdf-syntax-ns#]about");
+		}
+
+		@Override
+		public void handleParsedAttribute(int index, String value) {
+			assertEquals(ABOUTS[count], value);
+			OK_ABOUTS[count] = true;
+		}
 	}
 
 	class TitleRule extends DefaultRule {
 		public TitleRule() {
-			super(Type.CHARACTER, "/rss/channel/item/title");
+			super(
+					Type.CHARACTER,
+					"/[http://www.w3.org/1999/02/22-rdf-syntax-ns#]RDF/[http://purl.org/rss/1.0/]item/[http://purl.org/rss/1.0/]title");
 		}
 
 		@Override
@@ -68,41 +83,31 @@ public class ComplexTest extends AbstractTest {
 		}
 	}
 
-	class OrigLinkRule extends DefaultRule {
-		public OrigLinkRule() {
-			super(Type.CHARACTER,
-					"/rss/channel/item/[http://www.pheedo.com/namespace/pheedo]origLink");
+	class SubjectRule extends DefaultRule {
+		public SubjectRule() {
+			super(
+					Type.CHARACTER,
+					"/[http://www.w3.org/1999/02/22-rdf-syntax-ns#]RDF/[http://purl.org/rss/1.0/]item/[http://purl.org/dc/elements/1.1/]subject");
 		}
 
 		@Override
 		public void handleParsedCharacters(String text) {
-			assertEquals(ORIG_LINKS[count], text);
-			OK_ORIG_LINKS[count] = true;
+			assertEquals(SUBJECTS[count], text);
+			OK_SUBJECTS[count] = true;
 		}
 	}
 
-	class CreatorRule extends DefaultRule {
-		public CreatorRule() {
-			super(Type.CHARACTER,
-					"/rss/channel/item/[http://purl.org/dc/elements/1.1/]creator");
+	class DeptRule extends DefaultRule {
+		public DeptRule() {
+			super(
+					Type.CHARACTER,
+					"/[http://www.w3.org/1999/02/22-rdf-syntax-ns#]RDF/[http://purl.org/rss/1.0/]item/[http://purl.org/rss/1.0/modules/slash/]department");
 		}
 
 		@Override
 		public void handleParsedCharacters(String text) {
-			assertEquals(CREATORS[count], text);
-			OK_CREATORS[count] = true;
-		}
-	}
-
-	class PubDateRule extends DefaultRule {
-		public PubDateRule() {
-			super(Type.CHARACTER, "/rss/channel/item/pubDate");
-		}
-
-		@Override
-		public void handleParsedCharacters(String text) {
-			assertEquals(PUB_DATES[count], text);
-			OK_PUB_DATES[count++] = true;
+			assertEquals(DEPTS[count], text);
+			OK_DEPTS[count++] = true;
 		}
 	}
 }
