032b65f9e795269aece4c13dfaa57c0358abf109
Riyad Kalla <software@thebuzzmedia.com>
2011-02-22 19:04:35 -0700

    Added new test suite for all tests.

---
+++ src/test/java/com/thebuzzmedia/sjxp/AllTests.java
@@ -0,0 +1,14 @@
+package com.thebuzzmedia.sjxp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.thebuzzmedia.sjxp.rule.DefaultRuleTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ DefaultRuleTest.class, AttributeOnlyTest.class,
+		CharacterOnlyTest.class, ComplexTest.class, EmptyTest.class,
+		NamespaceTest.class, SimpleTest.class })
+public class AllTests {
+	// no op
+}
